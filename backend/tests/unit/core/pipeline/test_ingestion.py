edede266fb40b6eae409a1f8eca47733e8ab52a7
Randrianantenaina Bearisoa <rabearisoabex@gmail.com>
2026-03-10 22:05:07 +0300

    data ingestion testing improved

--- backend/tests/unit/core/pipeline/test_ingestion.py
+++ backend/tests/unit/core/pipeline/test_ingestion.py
@@ -1,18 +1,37 @@
-from src.core.pipeline.ingestion import run_data_ingestion
-from src.core.utils.helpers import get_data_sources, get_ingestion_destination
+import pytest
 from pathlib import Path
+from unittest.mock import patch
+from src.core.pipeline.ingestion import run_data_ingestion, get_data_sources
 
-destination = get_ingestion_destination()
-sources = [Path(destination)/file_name['name'] for file_name in get_data_sources()]
+# This fixture will be executed before each test to set up a temporary environment for testing 
+@pytest.fixture
+def mock_env(tmp_path):
+    # 1. Create a temporary directory to act as the ingestion destination
+    temp_destination = tmp_path / "data_lake"
+    temp_destination.mkdir()
 
-def test_run_data_ingestion():
+    # 2. Prepare the list of expected file paths
+    expected_files = [temp_destination / source['name'] for source in get_data_sources()]
+
+    # 3. Use 'patch' to make 'get_ingestion_destination' return our temporary directory
+    with patch("src.core.pipeline.ingestion.get_ingestion_destination", return_value=str(temp_destination)):
+        yield expected_files  # Ici, le test s'exécute
+    
+def test_run_data_ingestion(mock_env):
+    expected_files = mock_env
     run_data_ingestion()
-    assert all([file_path.exists() for file_path in sources])
+    
+    # 3. Assertions to check if the expected files have been created
+    assert all(f.exists() for f in expected_files)
 
-def test_run_data_ingestion_with_replace():
+def test_run_data_ingestion_with_replace(mock_env):
+    expected_files = mock_env
     run_data_ingestion(replace=True)
-    assert all([file_path.exists() for file_path in sources])
+    
+    assert all(f.exists() for f in expected_files)
 
-def test_run_data_ingestion_without_replace():
+def test_run_data_ingestion_without_replace(mock_env):
+    expected_files = mock_env
     run_data_ingestion(replace=False)
-    assert all([file_path.exists() for file_path in sources])
+    
+    assert all(f.exists() for f in expected_files)
