9c7a2ba06f5cb0bef7f27ae31ef82692f2b19d1c
Randrianantenaina Bearisoa <rabearisoabex@gmail.com>
2026-03-10 12:09:39 +0300

    Network/URL error handled

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+import requests
 from src.core.pipeline.ingestion import run_data_ingestion
 
 run_data_ingestion()
--- backend/src/core/pipeline/ingestion.py
+++ backend/src/core/pipeline/ingestion.py
@@ -1,5 +1,26 @@
+import requests
 from src.core.utils.helpers import get_data_sources, download_file, check_file_exist, get_ingestion_destination
 from pathlib import Path
+
+def data_ingestion_logic(replace: bool = True):
+    """
+    The logic begind the function run_data_ingestion()
+    """
+    destination = get_ingestion_destination()
+    data_sources = get_data_sources()
+    print(f"Starting data ingestion process to {destination}...")
+    for source in data_sources:
+        do_we_download = False
+        if replace:
+            do_we_download = True
+        else:
+            if not check_file_exist(source["name"], destination):
+                do_we_download = True
+            else:
+                print(f"✅ {source['name']} already exists in {destination}. Skipping download.")
+
+        if do_we_download:
+            download_file(source["url"], source["name"], Path(destination))
 
 def run_data_ingestion(replace: bool = True):
     """
@@ -9,15 +30,10 @@ def run_data_ingestion(replace: bool = True):
     Returns:
         None
     """
-    destination = get_ingestion_destination()
-    data_sources = get_data_sources()
-    for source in data_sources:
-        do_we_download = False
-        if replace:
-            do_we_download = True
-        else:
-            if not check_file_exist(source["name"], destination):
-                do_we_download = True
-
-        if do_we_download:
-            download_file(source["url"], source["name"], Path(destination))
+    try:
+        data_ingestion_logic(replace)
+        print("✅ Data ingestion process completed successfully.")
+    except requests.exceptions.ConnectionError:
+        print("🌐 Network error: Check your internet connection or the source URL.")
+    except Exception as e:
+        print(f"💥 An unexpected error occurred: {e}")
