f61046698fcec41abeab054e01102192b71680f1
Randrianantenaina Bearisoa <rabearisoabex@gmail.com>
2026-04-04 19:47:52 +0300

    feat: endpoints for get_features and post_predict ready

--- backend/src/api/main.py
+++ backend/src/api/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from src.core.inference.schemas import InferenceInput
+from src.core.inference.RicePricePredictor import RicePricePredictor
 
 app = FastAPI()
+predictor = RicePricePredictor()
 
-@app.get("/")
+@app.get("/features")
 async def root():
-    return {"message": "Hello World"}
+    return InferenceInput.model_json_schema()
+
+@app.post('/predict')
+async def predict(entry: InferenceInput):
+    predictor = RicePricePredictor()
+    prediction = predictor.predict(entry=entry)
+    return prediction
