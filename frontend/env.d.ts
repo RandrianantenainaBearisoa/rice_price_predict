48e985c87d6a51e2fdec14939691bd7833a3fa0b
Randrianantenaina Bearisoa <rabearisoabex@gmail.com>
2026-04-08 18:31:04 +0300

    feat: config & add vue router

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+// src/env.d.ts
+
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  // On définit que chaque fichier .vue exporte un composant typé
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
