9583abcbee7066d32692e8f35b2b3799caa2622b
Randrianantenaina Bearisoa <rabearisoabex@gmail.com>
2026-04-09 15:32:23 +0300

    feat: update main.py to include feature engineering and training steps in the pipeline

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
 import requests
 from src.core.pipeline.ingestion import run_data_ingestion
 from src.core.pipeline.cleaning import run_data_preparing
+from src.core.pipeline.feat_eng import run_feature_engineering
+from src.core.pipeline.training import run_training
 
 run_data_ingestion(replace=False)
 run_data_preparing()
+run_feature_engineering()
+run_training()
