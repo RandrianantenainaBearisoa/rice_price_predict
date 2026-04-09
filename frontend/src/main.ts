31c711579f3bbca1b6a659316bec573e55963607
Randrianantenaina Bearisoa <rabearisoabex@gmail.com>
2026-04-09 14:58:28 +0300

    feat: add Geologica Variable font and update theme styles

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+
+import '@fontsource-variable/geologica/full.css';
+
 import "../assets/styles/theme.scss"
 
 const app = createApp(App)
