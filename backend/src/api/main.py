ab4fa309551ccdc18724dc4f6895e611f37220aa
Randrianantenaina Bearisoa <rabearisoabex@gmail.com>
2026-04-09 15:32:04 +0300

    feat: add CORS middleware for API to handle cross-origin requests

--- backend/src/api/main.py
+++ backend/src/api/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.core.inference.schemas import InferenceInput
 from src.core.inference.RicePricePredictor import RicePricePredictor
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 predictor = RicePricePredictor()
 
 @app.get("/features")
